4f4e906697ceccf0c2eb104a31d144a2eaf383c4
z646930529 <35284110+646930529@users.noreply.github.com>
2021-05-11 14:12:39 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -19,13 +19,10 @@ def out(command):
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
